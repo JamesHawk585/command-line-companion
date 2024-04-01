673fafab4e4a86ba5ea5a211721ff4cabe3b4000
James Hawk <jph94880@gmail.com>
2024-03-31 19:13:16 -0500

    Adds searchTerm stateful value to Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import AddSnippetForm from "../AddSnippetForm/AddSnippetForm.jsx"
 import '../../App.css';
 
 
 const Header = ({ onSnippetAdded }) => {
+  const [searchTerm, setSearchTerm] = useState("")
 
   const dialogRef = useRef(null);
 
@@ -16,12 +17,18 @@ const Header = ({ onSnippetAdded }) => {
         onSnippetAdded(newSnippetObject)
     }
 
+    const handleSearch = (e) => {
+      console.log(e.target.value)
+      setSearchTerm(e.target.value)
+      console.log(searchTerm)
+    }
+
   return (
       <header className="header">
         <h1 id="cli-companion-logo">💻CLI-Companion</h1>
         <div>
           <label>
-            🔎 <input type="search" id="search-bar"></input>
+            🔎 <input type="search" id="search-bar" value={searchTerm} onChange={(e) => handleSearch(e)}></input>
             <button onClick={() => onAddButtonClick()}>+Add</button>
           </label>
         </div>
