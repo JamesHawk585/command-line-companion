dcb57e702df7231833156eb0514aa5d067e1b04a
James Hawk <jph94880@gmail.com>
2024-04-02 12:27:13 -0500

    Builds oput form in Authentication.jsx

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Authentication = () => {
-  return (
-    <div>Authentication</div>
-  )
-}
+  const [signUp, setSignUp] = useState(false);
 
-export default Authentication
+  const handleSignUpClick = () => {
+    return setSignUp((signUp) => !signUp);
+  };
+
+  const handleSubmit = () => {
+    console.log("handleSubmit");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          name="name"
+          value={"stateful boolean value here"}
+          onChange={console.log}
+        />
+        {signUp && (
+          <>
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              value={"value"}
+              onChange={console.log}
+            />
+          </>
+        )}
+        <input type="submit" value={signUp ? "Sign Up!" : "Log In!"}/>
+      </form>
+      <div className="auth-errors-switch-wrapper">
+        <h2 className="auth-errors">{"erros here!"}</h2>
+        <h2>{signUp ? "Already a member?" : "Not a member?"}</h2>
+        <button onClick={handleSignUpClick}>
+            {signUp ? "Log In!" : "Register Now!"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Authentication;
