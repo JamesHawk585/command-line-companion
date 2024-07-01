6103d0c71bfd442700ce6bdc814c4db20defc609
James Hawk <jph94880@gmail.com>
2024-07-01 15:11:09 -0500

    Adds basic dark mode to offCanvasNavBar

--- src/components/OffCanvasNavBar/OffCanvasNavBar.jsx
+++ src/components/OffCanvasNavBar/OffCanvasNavBar.jsx
@@ -42,48 +42,84 @@ function OffCanvasNavBar({ setSnippets, user, updateUser, fetchUser, setUser,pas
 
   return (
     <>
+    {darkMode === true? (
       <div className="menu-and-site-title">
-        {user === undefined ? (
+        {user === undefined? (
           <div id="hamburger-menu-placeholder"></div>
         ) : (
           <div className="hamburger-menu-wrapper" onClick={handleShow}>
             <GiHamburgerMenu size={48} />
           </div>
         )}
-
         <h1 className="site-title">Command Line Companion 💻</h1>
+        
+        <img src={lightModeIcon} alt="light mode" onClick={handleDarkModeClick} className="lightModeIcon"/>
 
-        {darkMode === true ? (
-          <img src={lightModeIcon} alt="light mode" onClick={handleDarkModeClick} className="lightModeIcon"/>
-        ) : (
-          <img src={darkModeIcon} alt="dark mode" onClick={handleDarkModeClick} className="darkModeIcon"/>
-        )}
-
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header className="close-button" closeButton>
+            <Offcanvas.Title></Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <ul>
+              <li>
+                <Link to="/" onClick={handleClose}>Home</Link>
+              </li>
+              <li>
+                <Link to="/UserProfile" onClick={handleClose}>
+                  Profile
+                </Link>
+              </li>
+              <li className="logout-button">
+                <Link to="/authentication" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </Offcanvas.Body>
+        </Offcanvas>
       </div>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header className="close-button" closeButton>
-          <Offcanvas.Title></Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <ul>
-            <li>
-              <Link to="/" onClick={handleClose}>Home</Link>
-            </li>
-            <li>
-              <Link to="/UserProfile" onClick={handleClose}>
-                Profile
-              </Link>
-            </li>
-            <li className="logout-button">
-              <Link to="/authentication" onClick={handleLogout}>
-                Logout
-              </Link>
-            </li>
-          </ul>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
+    ) : (
 
+      <div className="menu-and-site-title-dark">
+        {user === undefined? (
+          <div id="hamburger-menu-placeholder-dark"></div>
+        ) : (
+          <div className="hamburger-menu-wrapper-dark" onClick={handleShow}>
+            <GiHamburgerMenu size={48} />
+          </div>
+        )}
+        <h1 className="site-title-dark">Command Line Companion 💻</h1>
+        
+        <img src={darkModeIcon} alt="dark mode" onClick={handleDarkModeClick} className="darkModeIcon"/>
+
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header className="close-button-dark" closeButton>
+            <Offcanvas.Title></Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body className="offcanvas-body-dark">
+            <ul>
+              <li>
+                <Link to="/" onClick={handleClose} className="home-link-dark">Home</Link>
+              </li>
+              <li>
+                <Link to="/UserProfile" onClick={handleClose} className="profile-link-dark">
+                  Profile
+                </Link>
+              </li>
+              <li className="logout-button-dark">
+                <Link to="/authentication" onClick={handleLogout} className="logout-link-dark">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </div>
+    )}
+  </>
+  )
+}
+        
+        
+      
 export default OffCanvasNavBar;
