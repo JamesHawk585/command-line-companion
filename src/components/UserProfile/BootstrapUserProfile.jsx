b771c38cdbf1d5ea8a87fcd1eaa1cee833d75bbd
James Hawk <jph94880@gmail.com>
2024-07-08 17:40:02 -0500

    Adds template form in EditUserProfile.jsx

--- src/components/UserProfile/BootstrapUserProfile.jsx
+++ src/components/UserProfile/BootstrapUserProfile.jsx
@@ -1,12 +1,19 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import "./BootstrapUserProfile.css";
+import MugShot from "../../images/mugshot.png";
+import EditUserProfile from '../EditUserProfile/EditUserProfile';
+import React, { useState, useEffect, createContext } from "react";
 
 function BootstrapUserProfile({ user, lightMode, getClassNameSuffix }) {
+  const [editProfile, setEditProfile] = useState(false)
+
+console.log(editProfile)
 
     const {username, first_name, last_name, email} = user
 
     const editUserProfile = () => {
+      setEditProfile(true)
       console.log("edit user profile details!")
     }
 
@@ -16,14 +23,18 @@ function BootstrapUserProfile({ user, lightMode, getClassNameSuffix }) {
 
 
   return (
+    <>
+    {editProfile ? (
+      <EditUserProfile/>
+    ) : (
     <Card style={{ width: '18rem' }} className={`user-profile-card${getClassNameSuffix(lightMode)}`}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Img variant="top" src={MugShot} className='user-profile-picture'/>
       <Card.Body>
         <Card.Title>{username}</Card.Title>
-        {/* <Card.Text>
+        <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
-        </Card.Text> */}
+        </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroup.Item>{first_name}</ListGroup.Item>
@@ -35,6 +46,8 @@ function BootstrapUserProfile({ user, lightMode, getClassNameSuffix }) {
         <button onClick={() => deleteUser()} className={`delete-account-button${getClassNameSuffix(lightMode)}`}>Delete Account</button>
       </Card.Body>
     </Card>
+    )}
+    </>
   );
 }
 
