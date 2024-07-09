fbce668739061f146cabd01cdd93b5726d2a6dc6
James Hawk <jph94880@gmail.com>
2024-07-09 14:00:00 -0500

    Adds css to the user profile card in BootstrapUserProfile.jsx

--- src/components/UserProfile/BootstrapUserProfile.jsx
+++ src/components/UserProfile/BootstrapUserProfile.jsx
@@ -27,19 +27,19 @@ console.log(editProfile)
     {editProfile ? (
       <EditUserProfile/>
     ) : (
-    <Card style={{ width: '18rem' }} className={`user-profile-card${getClassNameSuffix(lightMode)}`}>
+    <Card className={`user-profile-card${getClassNameSuffix(lightMode)}`}>
       <Card.Img variant="top" src={MugShot} className='user-profile-picture'/>
       <Card.Body>
-        <Card.Title>{username}</Card.Title>
-        <Card.Text>
+        <Card.Title><h1>{username}</h1></Card.Title>
+        {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
-        </Card.Text>
+        </Card.Text> */}
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>{first_name}</ListGroup.Item>
-        <ListGroup.Item>{last_name}</ListGroup.Item>
-        <ListGroup.Item>{email}</ListGroup.Item>
+        <ListGroup.Item><h2>{first_name}</h2></ListGroup.Item>
+        <ListGroup.Item><h2>{last_name}</h2></ListGroup.Item>
+        <ListGroup.Item><h2>{email}</h2></ListGroup.Item>
       </ListGroup>
       <Card.Body>
         <button onClick={ () => editUserProfile()} className={`edit-profile-button${getClassNameSuffix(lightMode)}`}>Edit Profile</button>
